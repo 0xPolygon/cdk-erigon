f8257f2618985ac2a9e69837d059c2191a8be23c
Dumi Loghin <dumiloghin@gmail.com>
2025-03-12 14:33:18 +0800

    fix issue and improve test coverage for HashContractBytecodeBigInt()

--- smt/pkg/utils/util_test.go
+++ smt/pkg/utils/util_test.go
@@ -2,6 +2,8 @@ package utils
 
 import (
 	"bytes"
+	"crypto/rand"
+	"encoding/hex"
 	"fmt"
 	"math/big"
 	"reflect"
@@ -56,7 +58,7 @@ func BenchmarkHashContractBytecode(b *testing.B) {
 	str := strings.Repeat("e", 1000)
 	b.Run("1", func(b *testing.B) {
 		for n := 0; n < b.N; n++ {
-			HashContractBytecode(str)
+			HashContractBytecodeBigIntV1(str)
 		}
 	})
 	b.Run("2", func(b *testing.B) {
@@ -64,6 +66,56 @@ func BenchmarkHashContractBytecode(b *testing.B) {
 			HashContractBytecodeBigInt(str)
 		}
 	})
+	b.Run("3", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			HashContractBytecode(str)
+		}
+	})
+}
+
+// for input strings of even size
+func runTestHashContractBytecodeConsistencyEven(size int, t *testing.T) {
+	data := make([]byte, size)
+	rand.Read(data)
+	strData := hex.EncodeToString(data)
+	if !strings.HasPrefix(strData, "0x") {
+		strData = "0x" + strData
+	}
+	h1 := HashContractBytecodeBigIntV1(strings.ToLower(strData))
+	h2 := HashContractBytecodeBigInt(strData)
+	if h1.Cmp(h2) != 0 {
+		t.Errorf("(lower case) Expected %v, but got %v", h1, h2)
+	}
+	h3 := HashContractBytecodeBigInt(strings.ToUpper(strData[2:]))
+	if h1.Cmp(h3) != 0 {
+		t.Errorf("(upper case) Expected %v, but got %v", h1, h3)
+	}
+}
+
+// for input strings of odd size
+func runTestHashContractBytecodeConsistencyOdd(size int, t *testing.T) {
+	data := make([]byte, size)
+	rand.Read(data)
+	strData := hex.EncodeToString(data)
+	strData = strData[:len(strData)-1]
+	h1 := HashContractBytecodeBigIntV1(strings.ToLower(strData))
+	h2 := HashContractBytecodeBigInt(strData)
+	if h1.Cmp(h2) != 0 {
+		t.Errorf("(lower case) Expected %v, but got %v", h1, h2)
+	}
+	h3 := HashContractBytecodeBigInt(strings.ToUpper(strData))
+	if h1.Cmp(h3) != 0 {
+		t.Errorf("(upper case) Expected %v, but got %v", h1, h3)
+	}
+}
+
+func TestHashContractBytecodeConsistency(t *testing.T) {
+	runTestHashContractBytecodeConsistencyEven(1234, t)
+	runTestHashContractBytecodeConsistencyEven(37, t)
+	runTestHashContractBytecodeConsistencyEven(111, t) // edge case, the actual size is divisible by 56*2
+	runTestHashContractBytecodeConsistencyOdd(37, t)
+	runTestHashContractBytecodeConsistencyOdd(1, t)
+	runTestHashContractBytecodeConsistencyOdd(111, t) // edge case, the actual size is divisible by 56*2
 }
 
 func TestConvertBigIntToHex(t *testing.T) {
