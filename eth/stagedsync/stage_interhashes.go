0f2d1c12584e2b9d2002844fc54d1f692f4cdf6d
Elliot Hallam <elliot@revitt.consulting>
2025-10-29 10:43:03 +0000

    unwind to correct block when unwinding pmt on rpc

--- eth/stagedsync/stage_interhashes.go
+++ eth/stagedsync/stage_interhashes.go
@@ -134,7 +134,7 @@ func SpawnIntermediateHashesStage(s *StageState, u Unwinder, tx kv.RwTx, cfg Tri
 		}
 
 		if to > s.BlockNumber {
-			unwindTo := (to + s.BlockNumber) / 2 // Binary search for the correct block, biased to the lower numbers
+			unwindTo := s.BlockNumber // set unwind point to the point before current stage run
 			logger.Warn("Unwinding due to incorrect root hash", "to", unwindTo)
 			u.UnwindTo(unwindTo, BadBlock(headerHash, fmt.Errorf("incorrect root hash")))
 		}
